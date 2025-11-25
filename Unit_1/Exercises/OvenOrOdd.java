6d9f0b744e0f21471d1c3ce8ca35c61d0784984d
Ángel Hernández <aheresp688@gmail.com>
2025-11-25 21:23:57 +0100

    Add - OvenOrOdd

---
+++ Unit_1/Exercises/OvenOrOdd.java
@@ -0,0 +1,15 @@
+package Paquete_1;
+
+public class Par_O_Impar {
+
+	public static void main(String[] args) {
+
+		int num = 2;
+
+		if (num % 2 == 0) {
+			System.out.print("El número es par.");
+		} else {
+			System.out.print("El número es impar.");
+		}
+	}
+}
